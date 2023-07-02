c556c0703ab27dec84e4906761bf8384acb48c4b
Tirth Nagar <tirthnagar.n1@gmail.com>
2023-07-02 14:02:23 -0400

    solving logic complete

--- Sudoku Solver/source.c
+++ Sudoku Solver/source.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void initializeBoard(int sudoku[9][9], char input[81]) {
 
@@ -20,6 +21,7 @@ void initializeBoard(int sudoku[9][9], char input[81]) {
 }
 
 void printBoard(int sudoku[9][9]) {
+
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			printf("%d ", sudoku[i][j]);
@@ -28,6 +30,63 @@ void printBoard(int sudoku[9][9]) {
 	}
 }
 
+bool isValid(int sudoku[9][9], int row, int col, int choice) {
+	
+	// check if choice exists in col
+	for (int r = 0; r < 9; r++) {
+		if (choice == sudoku[r][col]) {
+			return false;
+		}
+	}
+
+	// check if choice exists in row
+	for (int c = 0; c < 9; c++) {
+		if (choice == sudoku[row][c]) {
+			return false;
+		}
+	}
+
+	// check if choice exists in 3x3 grid
+	int start_row = (row / 3) * 3;
+	int start_col = (col / 3) * 3;
+	
+	for (int r = start_row; r < start_row + 3; r++) {
+		for (int c = start_col; c < start_col + 3; c++) {
+			if (choice == sudoku[r][c]) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool solve(int sudoku[9][9], int r, int c) {
+
+	if (r == 9) {
+		return true;
+	}
+	else if (c == 9) {
+		return solve(sudoku, r + 1, 0);
+	}
+	else if (sudoku[r][c] != 0) {
+		return solve(sudoku, r, c + 1);
+	}
+	else {
+		for (int choice = 1; choice < 10; choice++) {
+			if (isValid(sudoku, r, c, choice)) {
+				sudoku[r][c] = choice;
+				if (solve(sudoku, r, c + 1)) {
+					return true;
+				}
+				sudoku[r][c] = 0;
+			}
+		}
+		return false;
+	}
+}
+
+
 int main() {
 
 	int sudoku[9][9];
@@ -35,6 +94,10 @@ int main() {
 	
 	initializeBoard(sudoku, input);
 	// printBoard(sudoku);
+
+	solve(sudoku, 0, 0);
+
+	printBoard(sudoku);
 
 	return 0;
 }
